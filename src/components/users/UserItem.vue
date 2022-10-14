6560f5b24722f09a64b895e7ed58e1fc9622e67f
mrOtoM <mrotomachala@gmail.com>
2022-10-14 13:22:00 +0200

    Doplnenie vetvy Users stylovanie

--- src/components/users/UserItem.vue
+++ src/components/users/UserItem.vue
@@ -11,4 +11,32 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+h3 {
+  margin: 0.5rem 0;
+}
+
+.role {
+  border-radius: 40px;
+  background-color: #ccc;
+  color: #252525;
+  padding: 0.25rem 1rem;
+  display: inline-block;
+}
+
+.role--engineer {
+  background-color: blue;
+  color: white;
+}
+
+.role--consultant {
+  background-color: #af003a;
+  color: white;
+}
+</style>
--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -18,4 +18,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
