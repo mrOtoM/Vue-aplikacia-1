ad0d100181dfa1a2767d1926e10f66500701a163
mrOtoM <mrotomachala@gmail.com>
2022-11-08 09:51:42 +0100

    implement scrollbehavior

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
 });
 
 export default router;
