8cf8299b02dce4bc1170c96d4d34217112776331
mrOtoM <mrotomachala@gmail.com>
2022-11-08 09:37:25 +0100

    different footers

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TeamsList from '../components/teams/TeamsList.vue';
 import UsersList from '../components/users/UsersList.vue';
 import TeamMembers from '../components/teams/TeamMembers.vue';
+import TeamsFooter from '../components/teams/TeamsFooter';
+import UsersFooter from '../components/users/UsersFooter';
 
 const routes = [
   {
@@ -15,13 +17,13 @@ const routes = [
         props: true,
       },
     ],
-    component: TeamsList,
+    components: { default: TeamsList, footer: TeamsFooter },
     alias: '/',
   },
   {
     path: '/users',
     name: 'UsersList',
-    component: UsersList,
+    components: { default: UsersList, footer: UsersFooter },
   },
   {
     path: '/:notFound(.*)',
